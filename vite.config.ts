7f879d9f69a0704658b40485fc4c87ccd33edf3e
Muhammad Ainur Rofiq <ainurmoh@gmail.com>
2025-06-06 08:30:18 +0700

    feat: react init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig(({ mode }) => {
+  // Load env vars based on current mode
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    server: {
+      host: env.VITE_HOST || '0.0.0.0',
+      port: parseInt(env.VITE_PORT) || 5173,
+    },
+    define: {
+      'process.env': env, // Optional: for accessing env vars in the app
+    },
+    plugins: [react()],
+    "compilerOptions": {
+      "types": ["vite/client", "node"]
+    }
+  };
+});
