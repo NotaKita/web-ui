fd137774b771b765dc1ca80c53ff6f57e439149c
Muhammad Ainur Rofiq <ainurmoh@gmail.com>
2026-02-24 15:47:09 +0700

    [FEAT] new invoice toko emas sabar

--- src/pages/Invoice/detail/invoiceDetail.tsx
+++ src/pages/Invoice/detail/invoiceDetail.tsx
@@ -5,15 +5,15 @@ const InvoiceDetail = () => {
   const invoiceItems = [
     {
       description: "New Menu - Pembelian Barcode",
-      unitCost: 350000,
+      unitCost: 300000,
       qty: 1,
-      price: 350000,
+      price: 300000,
     },
         {
       description: "New Menu - Pembelian Tanpa Surat",
-      unitCost: 350000,
+      unitCost: 300000,
       qty: 1,
-      price: 350000,
+      price: 300000,
     },
   ]
 
@@ -38,7 +38,7 @@ const InvoiceDetail = () => {
           <span className="text-md text-slate-500 font-light">Software Solutions</span>
         </div>
         <div className="text-right">
-          <h1 className="text-md font-bold text-gray-800 mb-2">INV-20260224-0019X7A8</h1>
+          <h1 className="text-md font-bold text-gray-800 mb-2">INV-20260224-0019X7A9</h1>
           <div className="text-xs text-gray-600 space-y-1">
             <div className="flex items-center justify-end space-x-2">
               <Calendar className="w-4 h-4" />
@@ -58,7 +58,7 @@ const InvoiceDetail = () => {
         <div>
           <h3 className="text-md font-semibold text-gray-800 mb-3">To:</h3>
           <div className="text-gray-600 space-y-1 text-xs">
-            <p className="font-medium">Toko Emas Sabar Jaya 2</p>
+            <p className="font-medium">Toko Emas Sabar</p>
             <p>Pasar Tumpang</p>
             <p>Kabupaten Malang, Jawa Timur</p>
             <p>Indonesia</p>
