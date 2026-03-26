620194587ae8df6c097d4c90a301045b3d2f0462
caelestia <caelestia@caelestias-MacBook-Air.local>
2026-03-26 11:00:45 +0700

    [FEAT] yearly

--- src/pages/Invoice/detail/invoiceDetail.tsx
+++ src/pages/Invoice/detail/invoiceDetail.tsx
@@ -4,16 +4,10 @@ const InvoiceDetail = () => {
 
   const invoiceItems = [
     {
-      description: "New Menu - Pembelian Barcode",
-      unitCost: 300000,
+      description: "Yearly - Domain, Hosting, Maintenance, Backup, Support",
+      unitCost: 3000000,
       qty: 1,
-      price: 300000,
-    },
-        {
-      description: "New Menu - Pembelian Tanpa Surat",
-      unitCost: 300000,
-      qty: 1,
-      price: 300000,
+      price: 3000000,
     },
   ]
 
@@ -38,15 +32,15 @@ const InvoiceDetail = () => {
           <span className="text-md text-slate-500 font-light">Software Solutions</span>
         </div>
         <div className="text-right">
-          <h1 className="text-md font-bold text-gray-800 mb-2">INV-20260224-0019X7A9</h1>
+          <h1 className="text-md font-bold text-gray-800 mb-2">INV-20260326-0019X7B1</h1>
           <div className="text-xs text-gray-600 space-y-1">
             <div className="flex items-center justify-end space-x-2">
               <Calendar className="w-4 h-4" />
-              <span>Date of Invoice: 24/02/2026</span>
+              <span>Date of Invoice: 26/03/2026</span>
             </div>
             <div className="flex items-center justify-end space-x-2">
               <FileText className="w-4 h-4" />
-              <span>Due Date: 03/03/2026</span>
+              <span>Due Date: 23/04/2026</span>
             </div>
           </div>
         </div>
