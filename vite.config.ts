9b3cad17683411b3db9ffdf7411f015793c6d4d2
caelestia <caelestia@caelestias-MacBook-Air.local>
2026-03-27 15:52:31 +0700

    [FEAT] change port to 5175

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
   return {
     server: {
       host: env.VITE_HOST || '0.0.0.0',
-      port: parseInt(env.VITE_PORT) || 5173,
+      port: parseInt(env.VITE_PORT) || 5175,
       allowedHosts: ['notakita.web.id'], // 👈 allow your domain
     },
     preview: {
