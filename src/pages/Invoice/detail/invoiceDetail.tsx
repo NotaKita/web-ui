85d5d2dd79a5d50b963d88635bfcd5d231e8ef4e
caelestia <ainurmoh@gmail.com>
2026-07-15 10:02:00 +0700

    [FEAT] harga maksimal - Rofiq

--- src/pages/Invoice/detail/invoiceDetail.tsx
+++ src/pages/Invoice/detail/invoiceDetail.tsx
@@ -4,10 +4,10 @@ const InvoiceDetail = () => {
 
   const invoiceItems = [
     {
-      description: "Addition of a weight-based filtering feature in the liabilities menu to simplify data sorting, improve transaction tracking, and enhance operational efficiency.",
-      unitCost: 150000,
+      description: "maximum price configuration and limiter on category and purchase menu",
+      unitCost: 300000,
       qty: 1,
-      price: 150000,
+      price: 300000,
     },
   ]
 
@@ -32,15 +32,15 @@ const InvoiceDetail = () => {
           <span className="text-md text-slate-500 font-light">Software Solutions</span>
         </div>
         <div className="text-right">
-          <h1 className="text-md font-bold text-gray-800 mb-2">INV-20260224-0019X7A8</h1>
+          <h1 className="text-md font-bold text-gray-800 mb-2">INV-20260224-0019X7A9</h1>
           <div className="text-xs text-gray-600 space-y-1">
             <div className="flex items-center justify-end space-x-2">
               <Calendar className="w-4 h-4" />
-              <span>Date of Invoice: 16/05/2026</span>
+              <span>Date of Invoice: 15/07/2026</span>
             </div>
             <div className="flex items-center justify-end space-x-2">
               <FileText className="w-4 h-4" />
-              <span>Due Date: 23/05/2026</span>
+              <span>Due Date: 22/07/2026</span>
             </div>
           </div>
         </div>
